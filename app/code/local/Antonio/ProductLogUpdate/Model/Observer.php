336aa46744024c530a52103a93c86b4a2ab741fe
antonio-valdez-arce <a.valdez.arce@gmail.com>
2016-05-08 17:22:38 +0200

    * fixed: comment and formating

--- app/code/local/Antonio/ProductLogUpdate/Model/Observer.php
+++ app/code/local/Antonio/ProductLogUpdate/Model/Observer.php
@@ -1,8 +1,8 @@
 <?php
 /* 
- * Our class name should follow the directory structure of our Observer.php model, 
+ * The class name should follow the directory structure of Observer.php model, 
  * starting from the namespace, replacing directory separators with underscores. 
- * The directory of ousr Observer.php is following:  app/code/local/Antonio/ProductLogUpdate/Model/Observer.php 
+ * The directory of Observer.php is following:  app/code/local/Antonio/ProductLogUpdate/Model/Observer.php 
  */
 class Antonio_ProductLogUpdate_Model_Observer
 {
@@ -10,8 +10,10 @@ class Antonio_ProductLogUpdate_Model_Observer
 	{
 		// Retrieve the product being updated from the event observer
 		$product = $observer->getEvent()->getProduct();
+
 		// Write a new line to var/log/product-updates.log
 		$name = $product->getName();
+		
 		$sku = $product->getSku();
 		Mage::log("{$name} ({$sku}) updated", null, 'product-updates.log');
 	}
